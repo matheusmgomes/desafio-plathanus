2cc20b8e37f65916fb913473d99e806c4fafdd4c
matheusmgomes <machado_gomes@hotmail.com>
2022-03-11 01:11:26 -0300

    Fixing model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,16 +1,8 @@
 class Property < ApplicationRecord
   has_many_attached :images
-  #validate :validate_images
+  validate :validate_images
 
   def cover
     images.third    
-  end
-  
-
-private
-  def validate_images
-    return if images.count > 3 and images.count <= 5
-
-    errors.add(:images, 'You can upload minimum 3 and max 5 images')
   end
 end
